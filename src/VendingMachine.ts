4b414a9998803d19b97b81e4c277ae623df9b771
Fujii-Tatsuya <tatsuya.fujii@litalico.co.jp>
2021-08-31 18:06:47 +0900

    210831 藤井

--- src/VendingMachine.ts
+++ src/VendingMachine.ts
@@ -1,14 +1,22 @@
 interface IVendingMachine {
-  insert: (money: number) => void;
+  total: number;
+  insert: (money: number) => number;
   refund: () => number;
 }
 
 export class VendingMachine implements IVendingMachine {
+  
+  private _storage: number = 0;
+
+  get total() {
+    return this._storage;
+  }
   /**
    * お金を投入する
    */
   insert(money: number) {
-    return;
+    this._storage += money;
+    return money;
   }
   /**
    * 払い戻し
--- test/VendingMachine.test.ts
+++ test/VendingMachine.test.ts
@@ -6,10 +6,20 @@ test("newできる", () => {
 
 test("お金を投入できる", () => {
   const vm = new VendingMachine();
-  expect(() => vm.insert(50)).not.toThrow()
+  expect(vm.insert(10)).toEqual(10);
+  expect(vm.insert(50)).toEqual(50);
+  expect(vm.insert(100)).toEqual(100);
+  expect(vm.insert(500)).toEqual(500);
+  expect(vm.insert(1000)).toEqual(1000);
+})
+
+test("投入金額", () => {
+  const vm = new VendingMachine();
+  expect(vm.total).toEqual(10);
 })
 
 test("払い戻しできる", () => {
   const vm = new VendingMachine();
-  expect(vm.refund()).toEqual(0)
+  expect(vm.refund()).toEqual(0);
+
 })
