44f65b6dac83686cdb65baa5f1408c2fa2a7b59e
tatsuya-fujii-LITALICO <tatsuya.fujii@litalico.co.jp>
2021-11-11 15:24:11 +0900

    貨幣種類を列挙できるようにリファクタリング

--- src/Money.ts
+++ src/Money.ts
@@ -1,2 +1,5 @@
-type Money = 1 | 5 | 10 | 50 | 100 | 500 | 1000 | 2000 | 5000 | 10000;
+const moneyKinds = [1, 5, 10, 50, 100, 500, 1000, 2000, 5000, 10000] as const;
+type Money = typeof moneyKinds[number];
+
+export { moneyKinds };
 export default Money;
