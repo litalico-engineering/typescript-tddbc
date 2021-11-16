6ef9de674e4676cf1be09c3f9972db816ac7b9ec
Akihiroooo <akihiro.ota@litalico.co.jp>
2021-11-16 22:31:18 +0900

    :art: レッドブル、水を定義

--- src/stock/Juice.ts
+++ src/stock/Juice.ts
@@ -11,3 +11,21 @@ export class Cola implements Juice {
     return 120;
   }
 }
+
+export class RedBull implements Juice {
+  get name(): string {
+    return "レッドブル";
+  }
+  get price(): number {
+    return 200;
+  }
+}
+
+export class Water implements Juice {
+  get name(): string {
+    return "水";
+  }
+  get price(): number {
+    return 100;
+  }
+}
