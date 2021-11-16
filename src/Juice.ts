5ccbffa30d67e299ae8152cbc15155c98be52d85
hikaru-inomoto-LITALICO <hikaru.inomoto@litalico.co.jp>
2021-11-16 17:45:25 +0900

    ジュース生成と在庫保存方式を変更
    テストは一切変えない

--- src/Juice.ts
+++ src/Juice.ts
@@ -1,18 +1,52 @@
+export type JuiceType = "コーラ" | "レッドブル" | "水";
+
 export class Juice {
-  name: string;
+  name: JuiceType;
   price: number; // 円
 
-  constructor(name: string, price: number) {
+  constructor(name: JuiceType, price: number) {
     this.name = name;
     this.price = price;
   }
-}
 
-export class Juices extends Array<Juice> {
-  public pickUp(name: string): Juice {
-    const index = this.findIndex((v) => v.name === name);
-    const juice = this[index];
-    this.splice(index, 1);
-    return juice;
+  static Generate(type: JuiceType): Juice {
+    if (type === "コーラ") return new Juice("コーラ", 120)
+    else if (type === "レッドブル") return new Juice("レッドブル", 200)
+    else if (type === "水") return new Juice("水", 100)
   }
 }
+
+export class JuiceLane {
+  private type: JuiceType;
+  private stock: Juice[] = [];
+
+  constructor(type: JuiceType, count?: number) {
+    this.type = type;
+    if (count) {
+      this.stock = [...Array(count)].map(() => Juice.Generate(this.type))
+    }
+  }
+
+  get canSupply() {
+    return this.stock.length > 0
+  }
+
+  get price() {
+    // TODO: 毎回生成してて環境に良くないのでなんとか
+    return Juice.Generate(this.type).price;
+  }
+
+  get array() {
+    return this.stock;
+  }
+
+  public restock(items: Juice[]) {
+    this.stock = this.stock.concat(items)
+  }
+
+  public pickup() {
+    return this.stock.pop()
+  }
+}
+
+export class Juices extends Array<Juice> { }
--- test/Juice.test.ts
+++ test/Juice.test.ts
@@ -1,4 +1,4 @@
-import { Juice, Juices } from "../src/Juice";
+import { Juice } from "../src/Juice";
 
 describe("new", () => {
   test("new コーラ", () => {
@@ -17,17 +17,5 @@ describe("new", () => {
     const juice = new Juice("水", 100);
     expect(juice.name).toEqual("水");
     expect(juice.price).toEqual(100);
-  });
-});
-
-describe("pickUp", () => {
-  test("コーラを排出する", () => {
-    // ↓juicesにJuiceオブジェクトをpushする
-    const juicesBeforePickUp = [new Juice("水", 100), new Juice("コーラ", 120), new Juice("レッドブル", 200)];
-    const juicesAfterPickUp = [new Juice("水", 100), new Juice("レッドブル", 200)];
-    const juices = new Juices(...juicesBeforePickUp);
-    const result = juices.pickUp("コーラ").name;
-    expect(result).toEqual("コーラ");
-    expect(juices).toEqual(juicesAfterPickUp);
   });
 });
